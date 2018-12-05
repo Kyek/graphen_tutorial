bd56817c5a1db9c14758c470fe8090b38004eee6
Manolo Rb <emanolorb@gmail.com>
2018-12-05 17:20:29 -0600

    relay example

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -17,7 +17,7 @@ class IngredientType(DjangoObjectType):
 
 class Query(object):
     all_categories = graphene.List(CategoryType)
-    all_ingredients = graphene.List(IngredientType)
+    all_ingredients = graphene.List(IngredientType, category=graphene.Int())
     category = graphene.Field(
         CategoryType, id=graphene.Int(), name=graphene.String())
     ingredient = graphene.Field(
@@ -28,7 +28,15 @@ class Query(object):
 
     def resolve_all_ingredients(self, info, **kwargs):
         # We can easily optimize query count in the resolve method
+        category = kwargs.get('category')
+
+        # subquery
+        if category is not None:
+            return Ingredient.objects.all().filter(category__pk=category)
+
         return Ingredient.objects.select_related('category').all()
+
+
 
     def resolve_category(self, info, **kwargs):
         id = kwargs.get('id')
@@ -53,3 +61,43 @@ class Query(object):
             return Ingredient.objects.get(name=name)
 
         return None
+
+# #relay
+# # 
+
+# # cookbook/ingredients/schema.py
+# from graphene import relay, ObjectType
+# from graphene_django import DjangoObjectType
+# from graphene_django.filter import DjangoFilterConnectionField
+
+# from .models import Category, Ingredient
+
+
+# # Graphene will automatically map the Category model's fields onto the CategoryNode.
+# # This is configured in the CategoryNode's Meta class (as you can see below)
+# class CategoryNode(DjangoObjectType):
+#     class Meta:
+#         model = Category
+#         filter_fields = ['name', 'ingredients']
+#         interfaces = (relay.Node, )
+
+
+# class IngredientNode(DjangoObjectType):
+#     class Meta:
+#         model = Ingredient
+#         # Allow for some more advanced filtering here
+#         filter_fields = {
+#             'name': ['exact', 'icontains', 'istartswith'],
+#             'notes': ['exact', 'icontains'],
+#             'category': ['exact'],
+#             'category__name': ['exact'],
+#         }
+#         interfaces = (relay.Node, )
+
+
+# class Query(object):
+#     category = relay.Node.Field(CategoryNode)
+#     all_categories = DjangoFilterConnectionField(CategoryNode)
+
+#     ingredient = relay.Node.Field(IngredientNode)
+#     all_ingredients = DjangoFilterConnectionField(IngredientNode) 
